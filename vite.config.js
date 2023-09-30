a4fae0ad76de0c4aa67c5d7f6cd5858496b410da
Timothy Liebermann <tliebermann.33@gmail.com>
2023-09-30 17:52:18 +0100

    Small changes for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000,
-    },
+	base: "/QuickTestData",
+	plugins: [react()],
+	server: {
+		port: 3000,
+	},
 });
